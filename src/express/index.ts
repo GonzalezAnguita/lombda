cf4467477242b4b3f4577e9e37db1e62a663aa03
José Tomás González <>
2024-11-12 10:02:45 -0300

    feat(Cors): add support for adding standard cors headers to responses

--- src/express/index.ts
+++ src/express/index.ts
@@ -4,7 +4,20 @@ import bodyParser from 'body-parser';
 import { adapter } from './adapter';
 import { LambdaRouteT } from '../types/lambda_route';
 
-export const ExpressLombda = (routes: LambdaRouteT[], config: { basePath: string, port: number }) => {
+type CorsHeadersT = {
+    'Access-Control-Allow-Origin': string,
+    'Access-Control-Allow-Headers': string,
+    'Access-Control-Allow-Methods': string,
+}
+
+export const ExpressLombda = (
+    routes: LambdaRouteT[],
+    config: {
+        basePath: string,
+        port: number,
+        cors?: CorsHeadersT,
+    },
+) => {
     const app = express();
 
     const router = adapter(config.basePath, routes);
@@ -12,8 +25,18 @@ export const ExpressLombda = (routes: LambdaRouteT[], config: { basePath: string
     app.use(bodyParser.text({ type: '*/*' }));
 
     app.use('*', router);
-    
+
+    app.use((_, res, next) => {
+        if (config.cors) {
+            Object.entries(config.cors).forEach(([key, value]) => {
+                res.append(key, value);
+            });
+        }
+
+        next();
+    });
+
     app.listen(config.port, () => {
-      console.log(`[server]: Lombda is running at http://localhost:${config.port}`);
+        console.log(`[server]: Lombda is running at http://localhost:${config.port}`);
     });
 }
